52b9d356645a888a6385d46f3b8bb5b4ea87bce4
Naomi Aro <naomi.aro@zalando.fi>
2018-10-06 17:32:57 +0300

    basic express stuff

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -4,3 +4,24 @@
 // - `/api/users/${id}` endpoint serves a single user data by ID
 // - `/api/users/xxx` serves 404 Not Found, as do all the other paths
 // - BONUS: Add GraphQL and GraphiQL endpoints for bonus
+
+const express = require('express');
+const data = require('./data/data');
+
+const PORT = 4848;
+const app = express();
+
+app.get('/api/users', function(req, res) {
+  res.json(data);
+});
+
+app.get('/api/users/:id', function(req, res) {
+  const id = req.params.id;
+  res.json(data[id]);
+});
+
+app.get('/*', function(req, res) {
+  res.sendStatus(404);
+});
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
